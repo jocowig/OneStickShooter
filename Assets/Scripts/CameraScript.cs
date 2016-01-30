39e18e81b02639019874c4984ff82ae46fa67647
Cole <jocowig@gmail.com>
2016-01-30 01:28:16 -0500

    Added player recoil movement
    projectile movement still not correct

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    public Vector3 myPos;
+    public Transform myPlay;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,22 +3,59 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
+    GameObject projectile;
     public float speed;
+    Rigidbody rb;
+    bool mouseDown;
+
+    Vector3 lookPos;
+    Vector3 lookDir;
 	// Use this for initialization
 	    void Start ()
     {
+        projectile = Resources.Load("Firework") as GameObject;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDown = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+        lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+        transform.LookAt(transform.position + lookDir, Vector3.up);
+        
     }
 
     void FixedUpdate ()
     {
-        if (Input.GetMouseButtonDown(0))
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(movement * speed / Time.deltaTime);
+        if(mouseDown)
         {
-            Vector3 shootDirection;
-            shootDirection = Input.mousePosition;
-            shootDirection.z = 0.0f;
-            shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
-            shootDirection = shootDirection - transform.position;
-            transform.position += shootDirection * speed * Time.deltaTime;
+            GameObject instProjectile = Instantiate(projectile) as GameObject;
+            instProjectile.transform.position = transform.position + new Vector3(transform.position.x, 0, transform.position.z);
+            instProjectile.transform.LookAt(transform.position + lookDir, Vector3.up);
+            Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+            projRB.AddForce(lookDir * speed / Time.deltaTime);
+            Vector3 negativeDir = new Vector3(-lookDir.x, 0, -lookDir.z);
+            rb.AddForce(negativeDir * speed / Time.deltaTime);
         }
     }
 }
