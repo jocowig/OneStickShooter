81d80393fe614f8a0c363cf9514463dba629bc6d
Cole <jocowig@gmail.com>
2016-01-29 22:08:33 -0500

    Basic Controls

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -3,17 +3,29 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
+    public Rigidbody rb;
+    public float speed;
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed / Time.deltaTime);
+
         Vector3 shootDirection;
-        shootDirection = Input.mousePosition;
-        shootDirection.z = 0.0f;
-        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
-        shootDirection = shootDirection - transform.position;
+          shootDirection = Input.mousePosition;
+          shootDirection.z = 0.0f;
+          shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+          shootDirection = shootDirection - transform.position;
+
+        rb.AddForce(-shootDirection * speed / Time.deltaTime);
     }
 }
