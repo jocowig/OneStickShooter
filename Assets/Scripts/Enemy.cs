f47688ccfbfe1f61afd1f8b18587317c266b3265
Cody Claborn <cxsquared@gmail.com>
2016-01-29 21:22:09 -0500

    Added basic enemy script :alien:

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject player;
+    public float followTime = 1f;
+    public float followSpeed = 2f;
+    private float elapsedTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (player != null)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > followTime)
+            {
+                iTween.MoveUpdate(player, player.transform.position, followSpeed);
+                elapsedTime = 0f;
+            }
+        }
+	}
+}
