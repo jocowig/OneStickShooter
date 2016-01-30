74a15f56d76f09ce6d71e072200b6f6d28d10637
Cody Claborn <cxsquared@gmail.com>
2016-01-29 23:31:38 -0500

    Added boss movement class

--- Assets/Scripts/BossEnemy.cs
+++ Assets/Scripts/BossEnemy.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class BossEnemy : MonoBehaviour {
 
+    public float speed = 10f;
+
 	// Use this for initialization
 	void Start () {
-	
+        GetComponent<Rigidbody>().velocity = new Vector3(speed, speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Vector3 norm = GetComponent<Rigidbody>().velocity.normalized;
+
+        norm.z = 0;
+
+        GetComponent<Rigidbody>().velocity = norm * speed;
+    }
 }
