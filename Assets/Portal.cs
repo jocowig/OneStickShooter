f4ff27404fb16125fca3e12e1b888e2782fe1a3f
Cody Claborn <cxsquared@gmail.com>
2016-01-31 12:00:43 -0500

    Added speed trap to level generation and portal sound.

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -4,17 +4,34 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour {
 
+	public AudioClip portalSound;
+
+	private float timer = .80f;
+	private float elapsed = 0;
+
+	bool playerEntered = false;
+
 	void OnTriggerEnter(Collider other)
 	{
 		//Debug.Log("Trap Collision Entered");
 		if (other.tag.Equals("Player"))
 		{
-			BossEnemy.speed += Random.Range (.5f, 2f);
-			PlayerScript.bossesKilled++;
-			PlayerScript.maxHealth += Random.Range (2, 4);
-			BossEnemy.maxHealth += Random.Range (5, 10);
-			LevelGenerator.RoomsToGenerate += Random.Range (2, 6);
-			SceneManager.LoadScene ("LevelGeneration");
+			AudioSource.PlayClipAtPoint (portalSound, this.transform.position);
+			playerEntered = true;
+		}
+	}
+
+	void Update() {
+		if (playerEntered) {
+			elapsed += Time.deltaTime;
+			if (elapsed > timer) {
+				BossEnemy.speed += Random.Range (.5f, 2f);
+				PlayerScript.bossesKilled++;
+				PlayerScript.maxHealth += Random.Range (2, 4);
+				BossEnemy.maxHealth += Random.Range (5, 10);
+				LevelGenerator.RoomsToGenerate += Random.Range (2, 6);
+				SceneManager.LoadScene ("LevelGeneration");
+			}
 		}
 	}
 }
